53318088b405b020f7442fac7b2bfbc30b3003af
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-16 19:32:21 +0000

    Changing approach to having separate functions for each button rather than on long function wiht many conditionals

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,36 @@
-'use strict';
-const $container = document.querySelector('.button-container');
-const $display = document.querySelector('.display');
-let first = true;
-let firstHalf = '';
-let secondHalf = '';
-let operation;
-const check = event => {
-  const target = event.target;
-  if (target.tagName !== 'BUTTON') { return; }
-  if (first) {
-    if (firstHalf.length > 9) { return; }
-    firstHalf += target.value;
-    $display.textContent = firstHalf;
-  } else if (!first) {
-    if (secondHalf.length > 9) { return; }
-    secondHalf += target.value;
-    $display.textContent = secondHalf;
-  } else {
-    operation = target.value;
-  }
-  if (operation === 'all-clear') {
-    first = true;
-    firstHalf = '';
-    secondHalf = '';
-    operation = '';
-    $display.textContent = '';
-  } else if (operation === 'clear-entry') {
-    if (first) { firstHalf = ''; } else { secondHalf = ''; }
-  }
-};
-// const runOperation = (first:string, second:string, operand:string) => {
-// }
-$container.addEventListener('click', check);
-// # sourceMappingURL=index.js.map
+// 'use strict';
+// const $numbers = document.querySelectorAll('.num');
+// const $operands = document.querySelectorAll('.operator');
+// const $clearEntry = document.querySelector('.clear-entry');
+// const $clearAll = document.querySelector('.all-clear');
+// const $display = document.querySelector('.display');
+// const first = true;
+// const firstHalf = '';
+// const secondHalf = '';
+// let operation;
+// // const check = (event: Event) => {
+// //     const target = event.target as HTMLInputElement;
+// //     if (target.tagName !== 'BUTTON') return;
+// //     if (first) {
+// //       if (firstHalf.length > 9) return;
+// //       firstHalf += target.value;
+// //       $display.textContent = firstHalf;
+// //     } else if (!first) {
+// //       if (secondHalf.length > 9) return;
+// //       secondHalf += target.value;
+// //       $display.textContent = secondHalf;
+// //     }
+// //     if (operation === 'all-clear') {
+// //       first = true;
+// //       firstHalf = '';
+// //       secondHalf = '';
+// //       operation = '';
+// //       $display.textContent = '';
+// //     } else if (operation === 'clear-entry') {
+// //       if (first) firstHalf = ''
+// //       else secondHalf = ''
+// //     }
+// //   }
+// // const runOperation = (first:string, second:string, operand:string) => {
+// // }
+// // # sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-const $container = document.querySelector('.button-container')!;
+const $numbers = document.querySelectorAll('.num')!;
+const $operands = document.querySelectorAll('.operator')!;
+const $clearEntry = document.querySelector('.clear-entry')!;
+const $clearAll = document.querySelector('.all-clear')!;
 const $display = document.querySelector('.display')!;
 
 let first:boolean = true;
@@ -6,34 +9,30 @@ let firstHalf:string = ''
 let secondHalf:string = ''
 let operation:string
 
-const check = (event: Event) => {
-    const target = event.target as HTMLInputElement;
-    if (target.tagName !== 'BUTTON') return;
-    if (first) {
-      if (firstHalf.length > 9) return;
-      firstHalf += target.value;
-      $display.textContent = firstHalf;
-    } else if (!first) {
-      if (secondHalf.length > 9) return;
-      secondHalf += target.value;
-      $display.textContent = secondHalf;
-    } else {
-      operation = target.value;
-    }
-    if (operation === 'all-clear') {
-      first = true;
-      firstHalf = '';
-      secondHalf = '';
-      operation = '';
-      $display.textContent = '';
-    } else if (operation === 'clear-entry') {
-      if (first) firstHalf = ''
-      else secondHalf = ''
-    }
-  }
+// const check = (event: Event) => {
+//     const target = event.target as HTMLInputElement;
+//     if (target.tagName !== 'BUTTON') return;
+//     if (first) {
+//       if (firstHalf.length > 9) return;
+//       firstHalf += target.value;
+//       $display.textContent = firstHalf;
+//     } else if (!first) {
+//       if (secondHalf.length > 9) return;
+//       secondHalf += target.value;
+//       $display.textContent = secondHalf;
+//     }
+//     if (operation === 'all-clear') {
+//       first = true;
+//       firstHalf = '';
+//       secondHalf = '';
+//       operation = '';
+//       $display.textContent = '';
+//     } else if (operation === 'clear-entry') {
+//       if (first) firstHalf = ''
+//       else secondHalf = ''
+//     }
+//   }
 
 // const runOperation = (first:string, second:string, operand:string) => {
 
 // }
-
-$container.addEventListener('click', check)

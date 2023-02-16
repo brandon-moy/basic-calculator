f4af4ccd00b7192f6392a9864f17276c108f722f
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-16 19:41:11 +0000

    Created function to updating numbers and separate function for clearing current entry

--- public/js/index.js
+++ public/js/index.js
@@ -1,36 +1,44 @@
-// 'use strict';
-// const $numbers = document.querySelectorAll('.num');
-// const $operands = document.querySelectorAll('.operator');
-// const $clearEntry = document.querySelector('.clear-entry');
-// const $clearAll = document.querySelector('.all-clear');
-// const $display = document.querySelector('.display');
-// const first = true;
-// const firstHalf = '';
-// const secondHalf = '';
-// let operation;
-// // const check = (event: Event) => {
-// //     const target = event.target as HTMLInputElement;
-// //     if (target.tagName !== 'BUTTON') return;
-// //     if (first) {
-// //       if (firstHalf.length > 9) return;
-// //       firstHalf += target.value;
-// //       $display.textContent = firstHalf;
-// //     } else if (!first) {
-// //       if (secondHalf.length > 9) return;
-// //       secondHalf += target.value;
-// //       $display.textContent = secondHalf;
-// //     }
-// //     if (operation === 'all-clear') {
-// //       first = true;
-// //       firstHalf = '';
-// //       secondHalf = '';
-// //       operation = '';
-// //       $display.textContent = '';
-// //     } else if (operation === 'clear-entry') {
-// //       if (first) firstHalf = ''
-// //       else secondHalf = ''
-// //     }
-// //   }
-// // const runOperation = (first:string, second:string, operand:string) => {
-// // }
-// // # sourceMappingURL=index.js.map
+'use strict';
+const $numbers = document.querySelectorAll('.num');
+// const $operands = document.querySelectorAll('.operator')!;
+const $clearEntry = document.querySelector('.clear-entry');
+// const $clearAll = document.querySelector('.all-clear')!;
+const $display = document.querySelector('.display');
+const first = true;
+let firstHalf = '';
+let secondHalf = '';
+// let operation:string = ''
+// const check = (event: Event) => {
+//     const target = event.target as HTMLInputElement;
+//     if (operation === 'all-clear') {
+//       first = true;
+//       firstHalf = '';
+//       secondHalf = '';
+//       operation = '';
+//       $display.textContent = '';
+//     } else if (operation === 'clear-entry') {
+//       if (first) firstHalf = ''
+//       else secondHalf = ''
+//     }
+//   }
+const updateNums = event => {
+  const target = event.target;
+  if (first) {
+    if (firstHalf.length > 9) { return; }
+    firstHalf += target.value;
+    $display.textContent = firstHalf;
+  } else if (!first) {
+    if (secondHalf.length > 9) { return; }
+    secondHalf += target.value;
+    $display.textContent = secondHalf;
+  }
+};
+const clearEntry = event => {
+  if (first) { firstHalf = ''; } else { secondHalf = ''; }
+  $display.textContent = '';
+};
+$numbers.forEach(number => {
+  number.addEventListener('click', updateNums);
+});
+$clearEntry.addEventListener('click', clearEntry);
+// # sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,16 @@
 const $numbers = document.querySelectorAll('.num')!;
-const $operands = document.querySelectorAll('.operator')!;
+// const $operands = document.querySelectorAll('.operator')!;
 const $clearEntry = document.querySelector('.clear-entry')!;
-const $clearAll = document.querySelector('.all-clear')!;
+// const $clearAll = document.querySelector('.all-clear')!;
 const $display = document.querySelector('.display')!;
 
 let first:boolean = true;
 let firstHalf:string = ''
 let secondHalf:string = ''
-let operation:string
+// let operation:string = ''
 
 // const check = (event: Event) => {
 //     const target = event.target as HTMLInputElement;
-//     if (target.tagName !== 'BUTTON') return;
-//     if (first) {
-//       if (firstHalf.length > 9) return;
-//       firstHalf += target.value;
-//       $display.textContent = firstHalf;
-//     } else if (!first) {
-//       if (secondHalf.length > 9) return;
-//       secondHalf += target.value;
-//       $display.textContent = secondHalf;
-//     }
 //     if (operation === 'all-clear') {
 //       first = true;
 //       firstHalf = '';
@@ -32,7 +22,27 @@ let operation:string
 //       else secondHalf = ''
 //     }
 //   }
+const updateNums = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  if (first) {
+    if (firstHalf.length > 9) return;
+    firstHalf += target.value;
+    $display.textContent = firstHalf;
+  } else if (!first) {
+    if (secondHalf.length > 9) return;
+    secondHalf += target.value;
+    $display.textContent = secondHalf;
+  }
+}
 
-// const runOperation = (first:string, second:string, operand:string) => {
+const clearEntry = (event: Event) => {
+  if (first) firstHalf = '';
+  else secondHalf = '';
+  $display.textContent = '';
+}
 
-// }
+$numbers.forEach(number => {
+  number.addEventListener('click', updateNums);
+})
+
+$clearEntry.addEventListener('click', clearEntry);

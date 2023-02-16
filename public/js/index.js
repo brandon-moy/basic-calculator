6d69062f7c178eb923a7965ee59e8286424e8ef1
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-16 19:17:54 +0000

    Created base function to determine if first part or second part of operation and to update calc display

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,35 @@
-// "use strict";
-// console.log('Hello World!');
-// //# sourceMappingURL=index.js.map
+'use strict';
+const $container = document.querySelector('.button-container');
+const $display = document.querySelector('.display');
+let first = true;
+let firstHalf = '';
+let secondHalf = '';
+let operation;
+const check = event => {
+  const target = event.target;
+  if (target.tagName !== 'BUTTON') { return; }
+  if (first) {
+    if (firstHalf.length > 9) { return; }
+    firstHalf += target.value;
+    $display.textContent = firstHalf;
+  } else if (!first) {
+    if (secondHalf.length > 9) { return; }
+    secondHalf += target.value;
+    $display.textContent = secondHalf;
+  } else {
+    operation = target.value;
+  }
+  if (operation === 'all-clear') {
+    first = true;
+    firstHalf = '';
+    secondHalf = '';
+    operation = '';
+    $display.textContent = '';
+  } else if (operation === 'clear-entry') {
+    if (first) { firstHalf = ''; } else { secondHalf = ''; }
+  }
+};
+// const runOperation = (first:string, second:string, operand:string) => {
+// }
+$container.addEventListener('click', check);
+// # sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log('Hello World!')
+const $container = document.querySelector('.button-container')!;
+const $display = document.querySelector('.display')!;
+
+let first:boolean = true;
+let firstHalf:string = ''
+let secondHalf:string = ''
+let operation:string
+
+const check = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    if (target.tagName !== 'BUTTON') return;
+    if (first) {
+      if (firstHalf.length > 9) return;
+      firstHalf += target.value;
+      $display.textContent = firstHalf;
+    } else if (!first) {
+      if (secondHalf.length > 9) return;
+      secondHalf += target.value;
+      $display.textContent = secondHalf;
+    } else {
+      operation = target.value;
+    }
+    if (operation === 'all-clear') {
+      first = true;
+      firstHalf = '';
+      secondHalf = '';
+      operation = '';
+      $display.textContent = '';
+    } else if (operation === 'clear-entry') {
+      if (first) firstHalf = ''
+      else secondHalf = ''
+    }
+  }
+
+// const runOperation = (first:string, second:string, operand:string) => {
+
+// }
+
+$container.addEventListener('click', check)
